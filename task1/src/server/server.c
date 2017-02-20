534eee4e303b111f72be7fecccddb1fa50449fd8
Domantas Jadenkus <djadenkus@gmail.com>
2017-02-21 01:23:23 +0200

    added /name command

--- task1/src/server/server.c
+++ task1/src/server/server.c
@@ -18,12 +18,86 @@ static void displayMessage(Server *server, const char *format, ...) {
 	sendToAll(server, buffer);
 }
 
+static void displayPrivateMessage(Server *server, Client *client, const char *format, ...) {
+	char buffer[MAX_MESSAGE_LENGTH + MAX_NAME_LENGTH + 10];
+
+	strcpy(buffer, "To: ");
+	strcpy(buffer + 4, client->name);
+	size_t nameLength = strlen(client->name);
+	strcpy(buffer + 4 + nameLength, " - ");
+
+	va_list v;
+	va_start(v, format);
+	vsnprintf(buffer + nameLength + 7, MAX_MESSAGE_LENGTH, format, v);
+	buffer[sizeof(buffer) - 1] = 0;
+	va_end(v);
+
+	sendMessage(server, client, buffer + nameLength + 7);
+	printColoredMessage(Magenta, buffer);
+}
+
+static bool isCommand(const char *command, const char *msg, const char **argStart, size_t *len) {
+	size_t commandLength = strlen(command);
+
+	if (strncmp(msg, command, commandLength) != 0) {
+		return false;
+	}
+
+	if (msg[commandLength] != ' ') {
+		return false;
+	}
+
+	size_t args = commandLength;
+	while (msg[args] == ' ') {
+		args++;
+	}
+
+	size_t lastNonSpace = args;
+	for (size_t pos = lastNonSpace; msg[pos] != 0; pos++) {
+		if (msg[pos] != ' ') {
+			lastNonSpace = pos;
+		}
+	}
+
+	*argStart = msg + args;
+	*len = lastNonSpace - args + 1;
+	return true;
+}
+
+static void executeCommand(Server *server, Client *client, const char *command) {
+	const char *argStart;
+	size_t argLen;
+	if (isCommand("name", command, &argStart, &argLen)) {
+		if (argLen > MAX_NAME_LENGTH) {
+			displayPrivateMessage(server, client,
+				"Name cannot be longer than %s charactars",
+				MAX_NAME_LENGTH);
+		} else {
+			char oldName[MAX_NAME_LENGTH];
+			strcpy(oldName, client->name);
+			memcpy(client->name, argStart, argLen);
+			client->name[argLen] = 0;
+			displayMessage(server, "%s%s is now %s%s",
+				yellow,
+				oldName,
+				client->name,
+				none);
+		}
+	} else {
+		displayPrivateMessage(server, client, "Unknown command");
+	}
+}
+
 void onMessage(Server *server, Client *client, const char *msg) {
-	displayMessage(server, "%s%s>%s %s",
-		yellow,
-		client->name,
-		none,
-		msg);
+	if (msg[0] == '/') {
+		executeCommand(server, client, msg + 1);
+	} else {
+		displayMessage(server, "%s%s>%s %s",
+			yellow,
+			client->name,
+			none,
+			msg);
+	}
 }
 
 void onConnected(Server *server, Client *client) {
